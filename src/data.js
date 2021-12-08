4fd9df2ed9c5e6a1f169623109e6831ece17d55b
LAM VU <lamvb411@gmail.com>
2021-12-08 16:32:10 +0700

    commit first time

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+      id: 1,
+      name: 'Pham Le Tuan Nam',
+      age: 22,
+      image:
+        'https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/254991473_2398410870296073_4210869890319037895_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=Ne7hjbOgevwAX_QiRsj&_nc_ht=scontent-sin6-2.xx&oh=e3a4ea8e6fdab65fb02c2ceb58e16360&oe=61B5F18E',
+    },
+    {
+      id: 2,
+      name: 'Nguyen Viet Duc',
+      age: 22,
+      image:
+        'https://scontent-sin6-3.xx.fbcdn.net/v/t39.30808-6/250481630_1862063967308896_205148589410048905_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=174925&_nc_ohc=QJFSr_Z7yXUAX_GI1gm&_nc_ht=scontent-sin6-3.xx&oh=e78f9793b38520cf8c58c4ca048a4f6c&oe=61B5C9AA',
+    },
+    {
+      id: 3,
+      name: 'Tom Halland',
+      age: 25,
+      image:
+        'https://scontent-sin6-2.xx.fbcdn.net/v/t1.6435-9/73077267_10156823528062613_7187329029110759424_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=730e14&_nc_ohc=qn3S_dMTQhMAX-9rinw&_nc_oc=AQlXF0vNYb02gTQ4D6cjgXkIizh8LCtck28oKWM619bNMTFrqh9nZ16Vkey6StsCKmjLl8ZIRX__Vr2PqElc27R6&_nc_ht=scontent-sin6-2.xx&oh=270fddb7df97d29e7298a9044be4b040&oe=61D783DD',
+    },
+    {
+      id: 4,
+      name: 'Tobey Maguire',
+      age: 46,
+      image:
+        'https://scontent-sin6-3.xx.fbcdn.net/v/t1.6435-9/164869023_294767495348625_439458051237460994_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=cEFjrfeerP0AX-PGSO3&tn=mzvKK6TwWm6xtrd2&_nc_ht=scontent-sin6-3.xx&oh=d6ae1fcaea7be6d29405388000a4a4f1&oe=61D553B5',
+    },
+    {
+      id: 5,
+      name: 'Son Tung MTP',
+      age: 27,
+      image:
+        'https://scontent-sin6-2.xx.fbcdn.net/v/t1.6435-9/176180721_314182680064132_7008643905822506439_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=-Q4Cl3xGV3QAX-FOGbe&tn=mzvKK6TwWm6xtrd2&_nc_ht=scontent-sin6-2.xx&oh=d83016b1f18d5c17fe47af8b1e479c35&oe=61D5CC88',
+    },
+  ];
